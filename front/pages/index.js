ce34564dd4cb7b1fce41d072e42046e0b63b9bd1
404_jamjam <jhy1025a@gmail.com>
2023-03-22 23:41:20 +0900

    404_nodebird_project_PostForm
    더미데이터를 활용하여 PostForm 구현
    이미지 업로드 버튼 Click시 file선택창 나오도록 구현

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import AppLayout from "../components/AppLayout";
+import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 
 const Home = () => {
+  const { isLoggedIn } = useSelector((state)=> state.user);
+  // useSelector()사용 하여 가져온다. 사용자의 로그인 여부 
+  const { mainPosts } = useSelector((state)=>state.post);
   return (
     <AppLayout>
-      <div>Hello, Next!</div>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => <PostCard key={post.id} post={post}/>)}
     </AppLayout>
   );
 };
--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,12 +1,59 @@
 export const initialState = {
-    mainPosts: [],
+    mainPosts: [{
+        id: 1,
+        User:{
+            id:1,
+            nickname: '사공사',
+        },
+        content: '첫 번째 게시글 #해시태그 #익스프레쓰',
+        Images: [{
+            src: 'https://images.unsplash.com/photo-1618183507099-4fa269f9b0ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' 
+        }, {
+            src: 'https://images.unsplash.com/photo-1544241907-f3f1f5ded15a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+        }],
+        Comments: [{
+            User: {
+                nickname: '버본',
+            },
+            content: '오 이제 시작인가?',
+        },{
+            User: {
+                nickname: '베르무트',
+            },
+            content: 'a secret makes a woman woman',
+        }]
+    }],
+    imagePaths: [],
+    postAdded: false,
 }
 
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+    type: ADD_POST,
+}
+const dummyPost = {
+    id: 2,
+    content: '더미데이터 인데요',
+    User: {
+        id: 1,
+        nickname: '사공사',
+    },
+    Images: [],
+    Comments:[],
+};
 
 const reducer = (state= initialState, action) => {
     switch (action.type) {
+        case ADD_POST:
+            return {
+                ...state,
+                mainPosts: [dummyPost, ...state.mainPosts],
+                postAdded: true,
+            };
         default:
-            return state;
+            return {
+                ...state,
+            }
     }
 };
 
