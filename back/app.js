bd2f720e733b759da075004e391aee9a345ce6db
404_jamjam <jhy1025a@gmail.com>
2023-04-12 21:27:21 +0900

    404_nodebird_project_back
    app.js 코드 수정 ( []제거)

--- back/app.js
+++ back/app.js
@@ -31,14 +31,14 @@ if (process.env.NODE_ENV === 'production') {
     app.use(hpp());
     app.use(helmet());
     app.use(cors({
-        origin: ['http://gongsabird.site'], //모든 출처 허용 옵션 , Access-Control-Allow-Origin 
+        origin: 'http://gongsabird.site', //모든 출처 허용 옵션 , Access-Control-Allow-Origin 
         // 쿠키공유까지 허락 되었을 때는 true or '*'로 사용할 수 없다. 민감한 정보보내니 정확한 주소 줘야한다..! 
         credentials: true,  //다른 도메인 간에 쿠키 공유를 허락하는 옵션 ,Access-Control-Allow-Credentials
     })); // CORS해결 
 } else {
     app.use(morgan('dev'));
     app.use(cors({
-        origin: ['http://localhost:3025'], //모든 출처 허용 옵션 , Access-Control-Allow-Origin 
+        origin: 'http://localhost:3025', //모든 출처 허용 옵션 , Access-Control-Allow-Origin 
         // 쿠키공유까지 허락 되었을 때는 true or '*'로 사용할 수 없다. 민감한 정보보내니 정확한 주소 줘야한다..! 
         credentials: true,  //다른 도메인 간에 쿠키 공유를 허락하는 옵션 ,Access-Control-Allow-Credentials
     })); // CORS해결 
