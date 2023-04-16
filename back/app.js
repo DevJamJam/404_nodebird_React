e7a9796f58fe046770a1c5332d7def60d6fafa84
404_jamjam <jhy1025a@gmail.com>
2023-04-16 17:19:16 +0900

    404_nodebird_project_back
    app..js 수정

--- back/app.js
+++ back/app.js
@@ -28,7 +28,7 @@ passportConfig();
 if (process.env.NODE_ENV === 'production') {
     app.use(morgan('combined'));
     app.use(hpp());
-    app.use(helmet({ contentSecurityPolicy: false }));
+    app.use(helmet());
     app.use(cors({
         origin: 'https://gongsabird.site', //모든 출처 허용 옵션 , Access-Control-Allow-Origin 
         // 쿠키공유까지 허락 되었을 때는 true or '*'로 사용할 수 없다. 민감한 정보보내니 정확한 주소 줘야한다..! 
